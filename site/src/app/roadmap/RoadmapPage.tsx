f4259e13c7a619db35fadf147506722b9c18b8c1
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2025-11-02 16:18:49 -0800

    feat: Auto-save roadmap changes

--- site/src/app/roadmap/RoadmapPage.tsx
+++ site/src/app/roadmap/RoadmapPage.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC, useRef } from 'react';
+import { FC, useRef, useEffect } from 'react';
 import './RoadmapPage.scss';
 import Planner from './planner/Planner';
 import SearchSidebar from './sidebar/SearchSidebar';
@@ -8,11 +8,28 @@ import AddCoursePopup from './planner/AddCoursePopup';
 import { useIsMobile } from '../../helpers/util';
 import { CSSTransition } from 'react-transition-group';
 import TransferCreditsMenu from './transfers/TransferCreditsMenu';
+import { useSaveRoadmap } from '../../hooks/planner';
+import { useIsLoggedIn } from '../../hooks/isLoggedIn';
 
 const RoadmapPage: FC = () => {
   const showSearch = useAppSelector((state) => state.roadmap.showSearch);
   const isMobile = useIsMobile();
   const sidebarRef = useRef(null);
+  const saveRoadmap = useSaveRoadmap();
+  const isLoggedIn = useIsLoggedIn();
+  const currentRevisionIndex = useAppSelector((state) => state.roadmap.currentRevisionIndex);
+  const savedRevisionIndex = useAppSelector((state) => state.roadmap.savedRevisionIndex);
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+    if (currentRevisionIndex === savedRevisionIndex) return;
+
+    const timeoutId = setTimeout(() => {
+      saveRoadmap(false); // false = don't show toast notifications
+    }, 2000);
+
+    return () => clearTimeout(timeoutId);
+  }, [currentRevisionIndex, savedRevisionIndex, isLoggedIn, saveRoadmap]);
 
   return (
     <>
